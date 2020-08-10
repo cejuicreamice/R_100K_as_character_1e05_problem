8819ee1e3c1f8c9f8222014081733caa935cbb0c
YIKIM Yjoan <59438814+cejuicreamice@users.noreply.github.com>
2020-08-10 09:19:01 +0900

    Create TEMP_FIX.R
    TEMP_FIX v0.1

---
+++ TEMP_FIX.R
@@ -0,0 +1,14 @@
+## TEMPORARY FIX Code is here:
+if (i %% 100000 == 0) {
+
+#another_digits = i %/% 100000
+# loop : i %/% 100000 -> i %/% 10 -> i %/% 10 -> i %/% 10 -> i %/% 10 do-while loop!!!!!
+
+#temp
+#another_digits <- 1
+# temp 100,000 ~ 900,000
+   digits_add <- "0"
+
+   conv_i_chr <- paste0(as.character(i %/% 100000), digits_add)
+
+}
